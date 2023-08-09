1e4e681fbd626af5dfde82576e8524fe1f351264
Michael Long <michaellong@rti-108543.rti.ns>
2023-08-09 15:12:18 -0400

    Add match example for Option

--- options/src/main.rs
+++ options/src/main.rs
@@ -3,6 +3,17 @@ fn main() {
     let number = Some(7);
     let letter: Option<i32> = None;
 
+    match number {
+        Some(i) => {
+            println!("This is a really long string and `{:?}`", i);
+            // ^ Needed 2 indentations just so we could destructure
+            // `i` from the option.
+        },
+        _ => {},
+        // ^ Required because `match` is exhaustive. Doesn't it seem
+        // like wasted space?
+    };
+
     // The `if let` construct reads: "if `let` destructures `number` into
     // `Some(i)`, evaluate the block (`{}`).
     if let Some(i) = number {
